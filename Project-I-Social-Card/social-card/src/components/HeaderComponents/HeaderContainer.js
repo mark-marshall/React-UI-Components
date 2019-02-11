50aa717ce94d9fa65cd718fd0ccd995cae6fe614
mark-marshall <mark.marshallgp@gmail.com>
2019-02-11 13:19:46 +0000

    [MVP] Added JSX for HeaderContainer components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+export default function HeaderContainer() {
+    return (
+        <div className="App">
+            <ImageThumbnail />
+            <HeaderTitle />
+            <HeaderContent />
+        </div>
+    )
+  }
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+export default function HeaderTitle() {
+    return(
+    <div>
+    <h1>Lambda School</h1>
+    <span>@LambdaSchool</span>
+    <span>·</span>
+    <span>26 Jan</span>
+    </div>
+    );
+}
