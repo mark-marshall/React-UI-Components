bf6b15e7128151c3c9b66607697c7ef983882292
mark-marshall <mark.marshallgp@gmail.com>
2019-02-11 13:49:41 +0000

    [MVP] Added JSX for CardContainer components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+export default function CardContainer() {
+    return (
+        <a href="https://www.reactjs.org" >
+        <div className="card-container">
+        <CardBanner />
+        <CardContent />
+        </div>
+        </a>
+    );
+}
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+export default function CardContent() {
+  return (
+    <div className="card-content">
+      <h1>Get started with React</h1>
+      <p className="content-intro">React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+      <p className="site-link">reactjs.org</p>
+    </div>
+  );
+}
