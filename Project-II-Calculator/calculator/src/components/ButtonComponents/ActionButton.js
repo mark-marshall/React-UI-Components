e5811605e5123eb9022b82f029eb202e27eabf89
mark-marshall <mark.marshallgp@gmail.com>
2019-02-11 15:14:00 +0000

    [MVP] Added ActionButton Component and styles

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Button.css';
+
+export default function ActionButton({ props }) {
+    return (
+        <button className="action-button">{props.text}</button>
+    );
+}
