9b25e5121c27ff92126be43e7bb84594fb8ecf90
mark-marshall <mark.marshallgp@gmail.com>
2019-02-12 12:53:42 +0000

    [STRETCH] Reformated dates from moment.js

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './Header.css';
 import moment from 'moment';
 
+const newDayFormat = moment().format('Do').replace(/th|st|rd/gi,'');
+const Date = `${newDayFormat} ${moment().format('MMM')}`
+
 export default function HeaderTitle() {
   return (
     <div className="header-title">
       <h1 className="main-heading">Lambda School</h1>
-      <div className="main-heading-items">@LambdaSchool · {moment().format('Do MMM')}</div>
+      <div className="main-heading-items">@LambaSchool · {Date}</div>
     </div>
   );
 }
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -4,3 +4,10 @@ import './Button.css';
 export default function ActionButton({ props }) {
   return <button className="action-button">{props.text}</button>;
 }
+
+
+/*
+export default ActionButton = props => {
+  return <button className="action-button">{props.text}</button>;
+}
+*/
